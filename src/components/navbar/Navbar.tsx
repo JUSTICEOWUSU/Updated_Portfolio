525ec13137205940ea4bb6c0c9a8e883f7aee88c
JUSTICEOWUSU <justicelearner2021@gmail.com>
2024-05-14 22:52:34 +0000

    updated code for better typography

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 function Navbar() {
   return (
     <div className="flex w-11/12 justify-between items-center capitalize mb-5 text-lg">
-      <span className="text-black font-semibold uppercase text-lg">
+      <span className="text-black font-semibold uppercase text-lg lg:text-xl">
         <a href="#home">
           Justice<span className="text-blue-500">{":>"}</span>
         </a>
       </span>
-      <span className="hover:text-blue-500 text-sm md:text-md">
+      <span className="hover:text-blue-500 text-sm md:text-md lg:text-lg">
         <a href="#projects">projects</a>
       </span>
-      <span className="hover:text-blue-500 text-sm md:text-md">
+      <span className="hover:text-blue-500 text-sm md:text-md lg:text-lg">
         <a href="#about">about</a>
       </span>
-      <span className="hover:text-blue-500 text-sm md:text-md">
+      <span className="hover:text-blue-500 text-sm md:text-md lg:text-lg">
         <a href="#contact">contact</a>
       </span>
     </div>
