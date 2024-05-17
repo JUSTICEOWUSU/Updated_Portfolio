f7305cc5c431a948d67411882103d5e93091bdda
JUSTICEOWUSU <justicelearner2021@gmail.com>
2024-05-17 20:33:03 +0000

    updated the projects arrangement

--- src/components/sections/projectSection/projectSection.tsx
+++ src/components/sections/projectSection/projectSection.tsx
@@ -3,8 +3,16 @@ import ProjectCard from '@/components/projectCard/ProjectCard'
 
 function ProjectSection() {
   return (
-    <div  id='projects' className='mt-12 lg:mt-0'>
+    <div id="projects" className="mt-12 lg:mt-0">
       <h2 className="text-lg uppercase mb-0">projects</h2>
+
+      <ProjectCard
+        name={"justice clothing"}
+        tools={"typescript + nodejs + css module"}
+        description={"E-commerce app for seamless online shopping "}
+        site={"https://justice-clothing.vercel.app/"}
+        code={"https://github.com/JUSTICEOWUSU/Justice-clothing"}
+      />
 
       <ProjectCard
         name={"linxmarketix"}
@@ -19,14 +27,6 @@ function ProjectSection() {
         description={"Mission control api for Nasa space exploration"}
         site={"https://nasa-mission-control-api.vercel.app/"}
         code={"https://github.com/JUSTICEOWUSU/NASA-MISSION-CONTROL-API"}
-      />
-
-      <ProjectCard
-        name={"justice clothing"}
-        tools={"typescript + nodejs + css module"}
-        description={"E-commerce app for seamless online shopping "}
-        site={"https://justice-clothing.vercel.app/"}
-        code={"https://github.com/JUSTICEOWUSU/Justice-clothing"}
       />
     </div>
   );
